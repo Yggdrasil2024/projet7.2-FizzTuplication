ee0ef483cd76d6c6a16e9c19a0e7b25e360e12d7
BIKOUTA Guyverna Mon-Mot Désir <bikoutaguyverna@gmail.com>
2026-07-25 20:39:35 +0100

    feat: tables.js terminé

--- tables.js
+++ tables.js
@@ -1,7 +1,19 @@
 // Le pont JS -> page. À recopier tel quel dans chaque projet S7 à S9.
-const sortie = document.querySelector("#sortie2");
+const sortie2 = document.querySelector("#sortie2");
 function afficher(html) {
- sortie.innerHTML += html;
+  sortie2.innerHTML += html;
 }
 
+const mutipliction = () => {
+  let htmlContent = ``;
+  for (let i = 1; i <= 9; i++) {
+    htmlContent += `<div class="mult"><h2 class="tabel">table de ${i}</h2>`;
+    for (let index = 1; index <= 10; index++) {
+      htmlContent += `<p>${index} * ${i} = ${index * i}</p>`;
+    }
+    htmlContent += `</div>`;
+  }
+  return htmlContent;
+};
 
+afficher(mutipliction());
